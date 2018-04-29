e5ef597d74f2ad88723e7c34d13ddc512e9cadc2
Zate <zate75@gmail.com>
2018-04-29 14:07:02 -0500

    init

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+	public int minZoom = 15;
+	public int maxZoom = 50;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+			GetComponent<Camera>().orthographicSize--;
+		}
+
+		if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+			GetComponent<Camera>().orthographicSize++;
+		}
+
+		if (GetComponent<Camera>().orthographicSize < minZoom) {
+			GetComponent<Camera>().orthographicSize = minZoom;
+		}
+
+		if (GetComponent<Camera>().orthographicSize > maxZoom) {
+			GetComponent<Camera>().orthographicSize = maxZoom;
+		}
+	}
+}
---
+++ Assets/Scripts/Player2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour {
+
+	//public float speed;
+	private Rigidbody2D rb2d;
+
+	private Vector3 startPos;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		MapGenerator mapGen = GetComponent<MapGenerator>();
+		startPos = mapGen.PlayerStart();
+		Debug.Log("playerx" + startPos.x );
+	}
+
+	void FixedUpdate()
+	{
+		// float moveHorizontal = Input.GetAxis ("Horizontal");
+		// float moveVertical = Input.GetAxis ("Vertical");
+		
+
+		// Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		// rb2d.AddForce (movement * speed);
+		// rb2d.AddForce (e.mousePosition * speed);
+		if (Input.GetMouseButton(0)) {
+			transform.position = Vector2.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.2f);
+			//Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.05f);
+			
+			Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			difference.Normalize();
+			// float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+			// transform.rotation = Quaternion.Euler(0f, 0f, rotation_z);
+			Vector2 movement = new Vector2 (difference.x, difference.y);
+			rb2d.AddForce (movement);
+			
+		}
+		rb2d.velocity = new Vector2(0,0);
+		
+	}
+}
